41bc05e0b1255bbfd87e905847411f41e8c16e15
montse <brossamorral.montse@gmail.com>
2022-10-23 17:03:26 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+
+def hi(name):
+    print("hi " + name + "!")
+
+girls = ["montse", "maria", "raquel", "anna"]
+for name in girls:
+    hi(name)
+    print("next girl")
+
+for i in range(1, 6):
+    print(i)
